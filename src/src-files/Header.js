5eda65ae051c460c8f9714a0acd6ed74aef54675
GamingSlayerNS <naxelalejandro@hotmail.com>
2023-05-24 03:20:16 -0500

    Projects section complete

--- src/src-files/Header.js
+++ src/src-files/Header.js
@@ -57,7 +57,6 @@ export default class header extends Component {
                 <div className="slideshow">
                     {/* <div className="hero-image"> */}
                         <video width="100%" autoPlay muted loop id="fieldVideo">
-                            {/* <source src="Field.mp4"></source> */}
                             <source src={images['Field.mp4']} type="video/mp4"></source>
                             <source src="https://youtu.be/X6DuFads1Pw" type="video/mp4"></source>
                             <p> Your browser doesn't support this video format. </p>
--- src/src-files/Projects.js
+++ src/src-files/Projects.js
@@ -8,7 +8,70 @@ function importAllImages(r) {
 }
 const images = importAllImages(require.context('./images', false, /\.(png|jpe?g|svg)$/));
 
-export default class projects extends Component {
+export default class projects extends Component {  
+    constructor(props) {
+        super(props);
+        this.changeSelection = this.changeSelection.bind(this);
+    }
+
+    componentDidMount() {
+        // Label all the project entries as " show", so they become visible
+        this.filterSelection("Show all");
+    }
+    
+    changeSelection = (btn) => {
+        var current = document.getElementsByClassName("btn active");
+        
+        if (current[0] != null) {
+            // console.log(current[0].className)
+            console.log(btn.textContent);
+            current[0].className = current[0].className.replace(" active", "");
+            btn.className += " active";
+
+            this.filterSelection(btn.textContent);
+        }
+    }
+
+    filterSelection(selection) {
+        var entries, i;
+        entries = document.getElementsByClassName("column");
+        if (selection === "Show all")
+            selection = "";
+
+        // console.log(entries);
+        // console.log(entries[0].className);
+        for (i = 0; i < entries.length; i++) {
+            this.hideSelection(entries[i], "show");
+            if (entries[i].className.indexOf(selection) > -1)
+                this.showSelection(entries[i], "show");
+        }
+    }
+
+    hideSelection(element, name) {
+        var i;
+        var arr1 = element.className.split(" ");
+        var arr2 = name.split(" ");
+
+        // console.log(arr1 + "  " + arr2);
+        for (i = 0; i < arr2.length; i++) {
+            if (arr1.indexOf(arr2[i]) > -1)
+                arr1.splice(arr1.indexOf(arr2[i]), 1);
+        }
+        element.className = arr1.join(" ");
+    }
+
+    showSelection(element, name) {
+        var i;
+        var arr1 = element.className.split(" ");
+        var arr2 = name.split(" ");
+
+        // console.log(arr1 + "  " + arr2)
+        for (i = 0; i < arr2.length; i++) {
+            if (arr1.indexOf(arr2[i]) === -1)
+                element.className += " " + arr2[i];
+        }
+    }
+    
     render() {
         return (
             <div>
@@ -20,14 +83,14 @@ export default class projects extends Component {
                             <h4>Reach your business goals with excellent user experience.</h4>
                         </div>
                         <div id="myBtnContainer">
-                            <button className="btn active"> Show all</button>
-                            <button className="btn"> UI/UX Design</button>
-                            <button className="btn"> Web Design</button>
-                            <button className="btn"> Graphic Design</button>
+                            <button className="btn active" onClick={e => this.changeSelection(e.target)}>Show all</button>
+                            <button className="btn" onClick={e => this.changeSelection(e.target)}>College Projects</button>
+                            <button className="btn" onClick={e => this.changeSelection(e.target)}>Websites</button>
+                            <button className="btn" onClick={e => this.changeSelection(e.target)}>3D Modeling</button>
                         </div>
     
                         <div className="row">
-                            <div className="column ui/ux design">
+                            <div className="column College Projects">
                                 <div className="content">
                                     <img style={{width: "100%", height: 150}} src={images['5c02b925747aaa04087bb958_iMac_2.jpg']} alt="Mountains" />
                                     <h5>Copart</h5>
@@ -37,7 +100,7 @@ export default class projects extends Component {
     
                                 </div>
                             </div>
-                            <div className="column ui/ux design">
+                            <div className="column College Projects">
                                 <div className="content">
                                     <img style={{width: "100%", height: 150}} src={images['5c02b925747aaa04087bb958_iMac_2.jpg']} alt="Lights" />
                                     <h5>Beltone</h5>
@@ -46,7 +109,7 @@ export default class projects extends Component {
                                     <em>01/01/2021</em>
                                 </div>
                             </div>
-                            <div className="column ui/ux design">
+                            <div className="column College Projects">
                                 <div className="content">
                                     <img style={{width: "100%", height: 150}} src={images['5c02b925747aaa04087bb958_iMac_2.jpg']} alt="Nature" />
                                     <h5>Hostgator</h5>
@@ -56,7 +119,7 @@ export default class projects extends Component {
                                 </div>
                             </div>
     
-                            <div className="column ui/ux design">
+                            <div className="column College Projects">
                                 <div className="content">
                                     <img style={{width: "100%", height: 150}} src={images['5c02b925747aaa04087bb958_iMac_2.jpg']} alt="Nature" />
                                     <h5>AutoSmart</h5>
@@ -66,7 +129,7 @@ export default class projects extends Component {
                                 </div>
                             </div>
     
-                            <div className="column ui/ux design">
+                            <div className="column College Projects">
                                 <div className="content">
                                     <img style={{width: "100%", height: 150}} src={images['5c02b925747aaa04087bb958_iMac_2.jpg']} alt="Nature" />
                                     <h5>CrashedToys</h5>
@@ -76,7 +139,7 @@ export default class projects extends Component {
                                 </div>
                             </div>
     
-                            <div className="column webdesign">
+                            <div className="column Websites">
                                 <div className="content">
                                     <img style={{width: "100%", height: 150}} src={images['5c02b92c747aaac1587bb959_iMac.jpg']} alt="Car" />
                                     <h5>Pet Shelter</h5>
@@ -85,7 +148,7 @@ export default class projects extends Component {
                                     <em>01/01/2021</em>
                                 </div>
                             </div>
-                            <div className="column webdesign">
+                            <div className="column Websites">
                                 <div className="content">
                                     <img style={{width: "100%", height: 150}} src={images['5c02b931747aaa1fb07bb95a_iPad.jpg']} alt="Car" />
                                     <h5>Auto Spa</h5>
@@ -94,7 +157,7 @@ export default class projects extends Component {
                                     <em>01/01/2021</em>
                                 </div>
                             </div>
-                            <div className="column webdesign">
+                            <div className="column Websites">
                                 <div className="content">
                                     <img style={{width: "100%", height: 150}} src={images['5c02b938747aaa5c9c7bb95b_iPhone_1.jpg']} alt="Car" />
                                     <h5>Floral PR</h5>
@@ -104,7 +167,7 @@ export default class projects extends Component {
                                 </div>
                             </div>
     
-                            <div className="column webdesign">
+                            <div className="column Websites">
                                 <div className="content">
                                     <img style={{width: "100%", height: 150}} src={images['5c02b93e770c2e396d3be540_iPhone_3.jpg']} alt="Car" />
                                     <h5>Michis Cakes</h5>
@@ -114,7 +177,7 @@ export default class projects extends Component {
                                 </div>
                             </div>
     
-                            <div className="column graphicdesign">
+                            <div className="column 3D Modeling">
                                 <div className="content">
                                     <img style={{width: "100%", height: 150}} src={images['5c02b947770c2e26b43be541_Macbook.jpg']} alt="People" />
                                     <h5>Florimania</h5>
@@ -123,7 +186,7 @@ export default class projects extends Component {
                                     <em>01/01/2021</em>
                                 </div>
                             </div>
-                            <div className="column graphicdesign">
+                            <div className="column 3D Modeling">
                                 <div className="content">
                                     <img style={{width: "100%", height: 150}} src={images['5c02b94b7d96506a602b6e75_Watch.jpg']} alt="People" />
                                     <h5>AFYSE</h5>
@@ -132,7 +195,7 @@ export default class projects extends Component {
                                     <em>01/01/2021</em>
                                 </div>
                             </div>
-                            <div className="column graphicdesign">
+                            <div className="column 3D Modeling">
                                 <div className="content">
                                     <img style={{width: "100%", height: 150}} src={images['5c02b94b7d96506a602b6e75_Watch.jpg']} alt="People" />
                                     <h5>Chorbie</h5>
