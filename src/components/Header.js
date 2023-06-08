d276dea84a029993f1be9e16da168df8f313b4ea
GamingSlayerNS <naxelalejandro@hotmail.com>
2023-06-08 18:58:44 -0500

    Dark mode update

--- src/components/Header.js
+++ src/components/Header.js
@@ -12,24 +12,55 @@ export default class header extends Component {
     componentDidMount() {
         // State var
         const theme = localStorage.getItem('theme');
+
         theme && document.body.classList.add(theme);
+        if (theme) {
+            this.setLight();
+        }
     }
 
-    darkMode = () => {
+    themeToggle = () => {
         const themeToggleBtns = document.querySelectorAll('#theme-toggle');
+        
     
         themeToggleBtns.forEach(btn => {
             document.body.classList.toggle('light-mode');
             if (document.body.classList.contains('light-mode')) {
                 localStorage.setItem('theme', 'light-mode');
+                this.setLight();
             } else {
                 localStorage.removeItem('theme');
-                document.body.removeAttribute('class')
+                document.body.removeAttribute('class');
+                this.setDark();
             }
         })
     }
 
-    handleMenu = () => {
+    setLight = () => {
+        const themeToggleImg = document.querySelector('#theme-toggle-img');
+        const themeLogo = document.querySelector('#theme-logo');
+        const themeGithub = document.querySelector('#theme-github');
+        const themeLinkedin = document.querySelector('#theme-linkedin');
+
+        themeLogo.src = images['SpeedyNAS Logo Light.png'];
+        themeToggleImg.src = images['icon-light moon.png'];
+        themeGithub.src = images['icon-light github.png'];
+        themeLinkedin.src = images['icon-light linkedin.png'];
+    }
+
+    setDark = () => {
+        const themeToggleImg = document.querySelector('#theme-toggle-img');
+        const themeLogo = document.querySelector('#theme-logo');
+        const themeGithub = document.querySelector('#theme-github');
+        const themeLinkedin = document.querySelector('#theme-linkedin');
+
+        themeLogo.src = images['SpeedyNAS Logo Dark.png'];
+        themeToggleImg.src = images['icon-dark sun.png'];
+        themeGithub.src = images['icon-dark github.png'];
+        themeLinkedin.src = images['icon-dark linkedin.png'];
+    }
+
+    handleMenuBurger = () => {
         const menuBtn = document.querySelector('.header__menu-burger');
         let menuOpen = false;
         if (menuBtn.classList.contains('open')) {
@@ -50,11 +81,11 @@ export default class header extends Component {
         return (
             <div className="container">
                 <div className="header__top-nav" id="header">
-                    <div className="header__menu-burger" onClick={this.handleMenu}>
+                    <div className="header__menu-burger" onClick={this.handleMenuBurger}>
                         <div className="header__bars"></div>
                     </div>
                     <div className="header__logo">
-                        <a href="#top"><img src={images['SpeedyNAS Logo Dark.png']} alt="Speedy logo dark" /></a>
+                        <a href="#top"><img id="theme-logo" src={images['SpeedyNAS Logo Dark.png']} alt="Speedy logo dark" /></a>
                     </div>
                     <menu>
                         <ul className="header__menu">
@@ -74,22 +105,20 @@ export default class header extends Component {
                     </menu>
                     <ul className="header__socials">
                         <li>
-                            {/* <button id="theme-toggle" onClick={this.darkMode}>
-                                <img className="header__theme-sun" src={images['icon-dark sun.png']} alt="toggle-theme"></img>
-                            </button> */}
-                            <button id="theme-toggle" onClick={this.darkMode}>
-                                <img className="header__theme-moon" src={images['icon-light moon.png']} alt="toggle-theme"></img>
+                            <button id="theme-toggle" onClick={this.themeToggle}>
+                                <img id="theme-toggle-img" className="header__theme-sun"
+                                src={images['icon-dark sun.png']} alt="toggle-theme"></img>
                             </button>
                         </li>
                         <li className="header__line"></li>
                         <li>
                             <a href="https://github.com/GamingSlayerNS" target="_blank" rel="noreferrer">
-                                <img src={images['icon-dark github.png']} alt="Github" />
+                                <img id="theme-github" src={images['icon-dark github.png']} alt="Github" />
                             </a>
                         </li>
                         <li>
                             <a href="https://www.linkedin.com/in/naxelsantiago/" target="_blank" rel="noreferrer">
-                                <img src={images['icon-dark linkedin.png']} alt="Linkedin" />
+                                <img id="theme-linkedin" src={images['icon-dark linkedin.png']} alt="Linkedin" />
                             </a>
                         </li>
                     </ul>
