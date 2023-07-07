127c17dfedfa45377f708537f0f76764f8402694
GamingSlayerNS <naxelalejandro@hotmail.com>
2023-07-07 18:34:56 -0500

    Hero Media Queries

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,12 +1,12 @@
 import { Component } from "react"
 import "../styles/style.css"
 
-function importAllImages(r) {
-    let images = {};
-    r.keys().forEach((item, index) => { images[item.replace('./', '')] = r(item); });
-    return images
-}
-const images = importAllImages(require.context('../images', false, /\.(png|jpe?g|svg)$/));
+// function importAllImages(r) {
+//     let images = {};
+//     r.keys().forEach((item, index) => { images[item.replace('./', '')] = r(item); });
+//     return images
+// }
+// const images = importAllImages(require.context('../images', false, /\.(png|jpe?g|svg)$/));
 
 export default class skills extends Component {
     render() {
