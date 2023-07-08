a6972d800195705a583b705227debf5189c9d419
GamingSlayerNS <naxelalejandro@hotmail.com>
2023-07-08 16:02:13 -0500

    Skills Section

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -11,12 +11,16 @@ const images = importAllImages(require.context('../images', false, /\.(png|jpe?g
 export default class skills extends Component {
     render() {
         return (
-            <div className="myskills section" id="skills">
+            <div className="skills section" id="skills">
                 <div className="container">
                     <h2>My Skills</h2>
                     <div className="colors-barS1" />
                     {/* <div className="colors-barS2" /><div className="colors-barS3" /> */}
-                    <img src={images['Skills.png']} alt="Skills List"></img>
+                    <div className="skills__img">
+                        <img id="skills-mobile" src={images['Skills-mobile.png']} alt="Skills List"></img>
+                        <img id="skills-tablet" src={images['Skills-tablet.png']} alt="Skills List"></img>
+                        <img id="skills-desktop" src={images['Skills-desktop.png']} alt="Skills List"></img>
+                    </div>
                 </div>
             </div>
         )
