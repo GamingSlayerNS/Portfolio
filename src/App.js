7d09ff357ef74fcb143213e1f8bc9ed2c0af613e
GamingSlayerNS <naxelalejandro@hotmail.com>
2023-10-18 22:33:16 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -82,7 +82,7 @@ export default class App extends Component{
 
     render() {
         //const isSmallScreen = this.state.width <= 1024
-        // Test 1.0
+        // Test 1.1
         return (
             <>
             <header className='Header'><Header handleMenuBurger={this.handleMenuBurger} /></header>
