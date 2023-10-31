19dc75627fe83a9abf15ab914d78e9cff8d081e6
Naxel Santiago <nas180011@cslinux1.utdallas.edu>
2023-10-31 16:34:31 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -82,7 +82,7 @@ export default class App extends Component{
 
     render() {
         //const isSmallScreen = this.state.width <= 1024
-        // Test 1.3
+        // Test 1.4
         return (
             <>
             <header className='Header'><Header handleMenuBurger={this.handleMenuBurger} /></header>
