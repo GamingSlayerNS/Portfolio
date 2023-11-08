45658ebdb9b6b462ede213654a9e314f33e4fd0b
SpeedyNAS <63477484+GamingSlayerNS@users.noreply.github.com>
2023-11-07 18:21:27 -0600

    Update Skills.js

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -17,11 +17,11 @@ export default class skills extends Component {
                     <div className="colors-barS1" />
                     {/* <div className="colors-barS2" /><div className="colors-barS3" /> */}
                     <div className="skills__img">
-                        <img className="lazy loading" id="skills-mobile" 
+                        <img id="skills-mobile" 
                         src="https://fakeimg.pl/375x2084" data-src={images['Skills-mobile.png']} alt="Skills List"></img>
-                        <img className="lazy loading" id="skills-tablet" 
+                        <img id="skills-tablet" 
                         src="https://fakeimg.pl/768x1182" data-src={images['Skills-tablet.png']} alt="Skills List"></img>
-                        <img className="lazy loading" id="skills-desktop" 
+                        <img id="skills-desktop" 
                         src="https://fakeimg.pl/1550x1041" data-src={images['Skills-desktop.png']} alt="Skills List"></img>
                     </div>
                 </div>
